bef37b5ef936619d3b0f82204b29806a9cae0f67
HomZSY <775384893@qq.com>
2016-06-03 22:15:20 +0800

    supplement

--- game.js
+++ game.js
@@ -68,7 +68,7 @@ var game = {
 			var LastScore = document.getElementById('LastScore');
 			LastScore.innerHTML = this.score;
 		}
-	}
+	},
 	/* 是否游戏结束 */
 	isGameOver: function(){  //1则游戏结束
 		if(!this.isFull()){
@@ -91,8 +91,80 @@ var game = {
 			}
 		}
 		return 1;
-	}
+	},
 	/* 是否能左移 */
+	canLeft: function(){ //1则可以左移
+		/* 如果有值的左边有一个是空的或者左边的值跟他一样就可以左移 */
+		for(var x = 0; x<=3; x++){ 
+			for(var y = 1; y<=3; y++){
+				if(this.data[x][y] != 0){
+					if(this.data[x][y-1] == 0 || this.data[x][y] == this.data[x][y-1]){
+						//左边相邻的一个为空或值相同时可以,因为是左到右遍历的，所以这样既可
+						return 1;
+					}
+				}
+			}
+		}
+		return 0;
+	},
+	/* 向左移动 */
+	moveLeft: function(){
+		if(canLeft){
+
+		}
+	},
+	/* 是否能右移 */
+	canRight: function(){ //1则可以右移
+		/* 如果有值的右边有一个是空的或者右边的值跟他一样就可以右移 */
+		for(var x = 0; x<=3; x++){ 
+			for(var y = 2; y>=0; y--){
+				if(this.data[x][y] != 0){
+					if(this.data[x][y+1] == 0 || this.data[x][y] == this.data[x][y+1]){
+						return 1;
+					}
+				}
+		
+	/* 是否能上移 */
+	canUp: function(){ //1则可以上移
+		/* 如果有值的上边有一个是空的或者上边的值跟他一样就可以上移 */
+		for(var y = 0; y<=3; y++){ 
+			for(var x = 1; x<=3; x++){
+				if(this.data[x][y] != 0){
+					if(this.data[x-1][y] == 0 || this.data[x][y] == this.data[x-1][y]){
+						return 1;
+					}
+				}
+			}
+		}
+		return 0;
+	},
+	/* 向左移动 */
+	moveUp: function(){
+		if(canUp){
+
+		}
+	},
+	/* 是否能下移 */
+	canDown: function(){ //1则可以下移
+		/* 如果有值的下边有一个是空的或者下边的值跟他一样就可以下移 */
+		for(var y = 0; y<=3; y++){ 
+			for(var x = 2; x>=0; x--){
+				if(this.data[x][y] != 0){
+					if(this.data[x+1][y] == 0 || this.data[x][y] == this.data[x+1][y]){
+						return 1;
+					}
+				}
+			}
+		}
+		return 0;
+	},
+	/* 向下移动 */
+	moveDown: function(){
+		if(canDown){
+			
+		}
+	},	
+
 }
 window.onload = function(){
 	game.start();
